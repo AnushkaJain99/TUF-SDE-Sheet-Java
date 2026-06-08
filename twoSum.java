50813a77e217a7010b093cf5ba26ee6a972d3e7e
AnushkaJain99 <anushkatares405@gmail.com>
2026-06-08 17:49:09 +0530

    Day 07 completed

---
+++ twoSum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class twoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int rem = target - nums[i];
+
+            if (map.containsKey(rem)) {
+                return new int[]{map.get(rem), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] ans = twoSum(nums, target);
+
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
